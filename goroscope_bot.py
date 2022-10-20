3c827899a4d6f94a57d721cf4e5119d1c3804b76
MastenaM <mastena112003@gmail.com>
2022-10-20 23:02:01 +0300

    The horoscope was modified

--- goroscope_bot.py
+++ goroscope_bot.py
@@ -1,142 +1,130 @@
-# Подключаем модуль для Телеграма
+
 import telebot
-#заготовки
-# Подключаем модуль случайных чисел
+
 import random
 
-# Указываем токен
+
 bot = telebot.TeleBot('5615645123:AAF9qf79ga9BGPdNjjWQT58XRH9ajQ7dIEU')
 
-# Импортируем типы из модуля, чтобы создавать кнопки
+
 from telebot import types
 
-# Заготовки для трёх предложений
 
-first = ["Today is the perfect day for new beginnings.",
-         "The optimal day to decide on a bold act!",
-         "Be careful, today the stars can affect your financial condition.",
-         "The best time to start a new relationship or sort out old ones.",
-         "A fruitful day to sort out the accumulated cases."]
+first = ["The best day to study the subject. And the last one.",
+         "Don't teach. You can't.",
+         "Be careful, the subject can affect your mental condition.",
+         "The best time to start a new task or finish old ones.",
+         "There are still many incomprehensible things in the world. Leave this one alone."]
 
 second = ["But remember that even in this case, you should not forget about",
-          "If you go out of town, think about it in advance",
+          "If you go out of town, return back and think about",
           "Those who are aiming to do a lot of things today should remember about",
           "If you have a breakdown, pay attention to",
           "Remember that thoughts are material, which means that you need to constantly think about"]
 
-second_add = ["relationships with friends and family.",
-              "work and business issues that can interfere with plans so inopportunely.",
-              "yourself and your health, otherwise a complete breakdown is possible by the evening.",
-              "household issues — especially those that you did not finish yesterday.",
-              "rest, so as not to turn yourself into a hunted horse at the end of the month."]
+second_add = ["Python and Python.",
+              "quizzes that can interfere with plans so inopportunely.",
+              "your mental health, otherwise a complete breakdown is possible by the evening.",
+              "deadlines — especially those that you did not finish yesterday.",
+              "the rest?"]
 
-third = ["Evil tongues may tell you the opposite, but you don't need to listen to them today.",
-         "Know that success favors only the persistent, so dedicate this day to the education of the spirit.",
-         "Even if you cannot reduce the influence of retrograde Mercury, then at least bring things to an end.",
-         "There is no need to be afraid of lonely meetings — today is the time when they mean a lot.",
-         "If you meet a stranger on the way, take part, and then this meeting will promise you pleasant troubles."]
+third = ["Evil tongues may tell you this, but you don't need to listen to them today.",
+         "Know that success favors only the persistent, so dedicate this day to the education of the Python.",
+         "Even if you cannot reduce the influence of retrograde Mercury, don't think about deadlines.",
+         "There is no need to be afraid of functions — today is the time when they mean a lot.",
+         "If you meet professor on the way, take part, and then this meeting will promise you pleasant troubles."]
 
 
-# Метод, который получает сообщения и обрабатывает их
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    # Если написали «/start»
+
     if message.text == "/start":
 
-        # Пишем приветствие
 
-        bot.send_message(message.from_user.id, "Hi! I can tell you your horoscope for today ")
 
-        # Готовим кнопки
+        bot.send_message(message.from_user.id, "Hi! I can predict your succses in the subject and give you a piece of advice.")
+
+
 
         keyboard = types.InlineKeyboardMarkup()
 
-        # По очереди готовим текст и обработчик для каждого знака зодиака
 
-        key_aries = types.InlineKeyboardButton(text='Aries', callback_data='zodiac')
+        key_datam = types.InlineKeyboardButton(text='Data management in database design', callback_data='subject')
 
-        # И добавляем кнопку на экран
 
-        keyboard.add(key_aries)
+        keyboard.add(key_datam)
 
-        key_taurus = types.InlineKeyboardButton(text='Taurus', callback_data='zodiac')
+        key_python = types.InlineKeyboardButton(text='Programming in Python', callback_data='subject')
 
-        keyboard.add(key_taurus)
+        keyboard.add(key_python)
 
-        key_gemini = types.InlineKeyboardButton(text='Gemini', callback_data='zodiac')
+        key_stat = types.InlineKeyboardButton(text='Mathematical Statistics', callback_data='subject')
 
-        keyboard.add(key_gemini)
+        keyboard.add(key_stat)
 
-        key_cancer = types.InlineKeyboardButton(text='Cancer', callback_data='zodiac')
+        key_account = types.InlineKeyboardButton(text='Financial and Organizational Accounting', callback_data='subject')
 
-        keyboard.add(key_cancer)
+        keyboard.add(key_account)
 
-        key_leo = types.InlineKeyboardButton(text='Leo', callback_data='zodiac')
+        key_bank = types.InlineKeyboardButton(text='Fundamentals of Banking', callback_data='subject')
 
-        keyboard.add(key_leo)
+        keyboard.add(key_bank)
 
-        key_virgo = types.InlineKeyboardButton(text='Virgo', callback_data='zodiac')
+        key_systems = types.InlineKeyboardButton(text='Introduction to information systems', callback_data='subject')
 
-        keyboard.add(key_virgo)
+        keyboard.add(key_systems)
 
-        key_libra = types.InlineKeyboardButton(text='Libra', callback_data='zodiac')
+        key_ethics = types.InlineKeyboardButton(text='Business Ethics and Corporate Social Responsibility', callback_data='subject')
 
-        keyboard.add(key_libra)
+        keyboard.add(key_ethics)
 
-        key_scorpio = types.InlineKeyboardButton(text='Scorpio', callback_data='zodiac')
+        key_infrastruct = types.InlineKeyboardButton(text='Digital infrastructures for business', callback_data='subject')
 
-        keyboard.add(key_scorpio)
+        keyboard.add(key_infrastruct)
 
-        key_sagittarius = types.InlineKeyboardButton(text='Sagittarius', callback_data='zodiac')
+        key_iso = types.InlineKeyboardButton(text='Information Systems and Organisations', callback_data='subject')
 
-        keyboard.add(key_sagittarius)
+        keyboard.add(key_iso)
 
-        key_capricorn = types.InlineKeyboardButton(text='Capricorn', callback_data='zodiac')
+        key_architec = types.InlineKeyboardButton(text='Enterprise Architecture', callback_data='subject')
 
-        keyboard.add(key_capricorn)
+        keyboard.add(key_architec)
 
-        key_aquarius = types.InlineKeyboardButton(text='Aquarius', callback_data='zodiac')
+        key_globe = types.InlineKeyboardButton(text='Business and Management in Global Context', callback_data='subject')
 
-        keyboard.add(key_aquarius)
+        keyboard.add(key_globe)
 
-        key_pisces = types.InlineKeyboardButton(text='Pisces', callback_data='zodiac')
+        key_learn = types.InlineKeyboardButton(text='Machine Learning with Python', callback_data='subject')
 
-        keyboard.add(key_pisces)
+        keyboard.add(key_learn)
 
-        # Показываем все кнопки сразу и пишем сообщение о выборе
-        bot.send_message(message.from_user.id, text="Choose your zodiac sign:", reply_markup=keyboard)
+
+        bot.send_message(message.from_user.id, text="Choose the subject:", reply_markup=keyboard)
 
     elif message.text == "/help":
 
         bot.send_message(message.from_user.id,
-                         "This bot can predict your horoscope for today. To get started, write /start")
+                         "This bot can predict your success in the subject. To get started, write /start")
 
     else:
 
         bot.send_message(message.from_user.id, "I don't understand you. Please write /help.")
 
 
-# Обработчик нажатий на кнопки
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    # Если нажали на одну из 12 кнопок — выводим гороскоп
 
-    if call.data == "zodiac":
-        # Формируем гороскоп
+    if call.data == "subject":
 
         msg = random.choice(first) + ' ' + random.choice(second) + ' ' + random.choice(
             second_add) + ' ' + random.choice(third)
 
-        # Отправляем текст в Телеграм
 
         bot.send_message(call.message.chat.id, msg)
 
 
-# Запускаем постоянный опрос бота в Телеграме
-
-#запускаем бота
 def main_loop():
     while True:
         bot.polling(none_stop=True, interval=0)
